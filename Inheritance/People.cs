7fd62cec03f8c95081da6e68a784a604933179ad
Maggie Freemyer-Ackerman <mfreemyer@gmail.com>
2022-04-19 18:59:43 -0500

    Inheritance -- people and program working so far (want to add writelines for people though)

---
+++ Inheritance/People.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Inheritance
+{
+
+    class People // base class (parent)
+    {
+        protected string FName;
+        protected string LName;
+        protected string Pronouns;
+        protected string PType;
+
+        // This is the default constructor when no values are being passed. 
+        public People ()
+        {
+            FName = null;
+            LName = null;
+            Pronouns = null;
+            PType = null;
+        }
+
+        // This is the constructor when values are passed.
+        public People (string newFName, string newLName, string newPronouns, string newPType)
+        {
+            FName = newFName;
+            LName = newLName;
+            Pronouns = newPronouns;
+            PType = newPType;
+        }
+
+
+        // Since FName, LName and Pronouns aren't defined as properties, I need to create the get and set methods for them.
+        public string GetFName()
+        {
+            return FName;
+        }
+
+        public void SetFName (string newFName)
+        {
+            FName = newFName;
+        }
+
+       
+        // get, set LName
+        public string GetLName()
+        {
+            return LName;
+        }
+
+        public void SetLName (string newLName)
+        {
+            LName = newLName;
+        }
+
+        // get, set Pronouns
+        public string GetPronouns()
+        {
+            return Pronouns;
+        }
+
+        public void SetPronouns (string newPronouns)
+        {
+            Pronouns = newPronouns;
+        }
+
+        // get, set PType
+        public string GetPType()
+        {
+            return PType;
+        }
+
+        public void SetPType (string newPType)
+        {
+            PType = newPType;
+        }
+
+        
+        // ToString
+        public override string ToString()
+        {
+            return FName + " " + LName + "  Preferred Pronouns: " + Pronouns + "  Type: " + PType;
+        }
+
+
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Inheritance
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //delare and instantiate the array of people
+            People[] peopleArray=new People[200];
+
+            //loop through each array element and instantiate an object for each. 
+            for (int index = 0; index < peopleArray.Length; index++)
+            {
+                peopleArray[index] = new People();
+            }
+
+            //load some test data
+            peopleArray[0].SetFName("Maggie");
+            peopleArray[0].SetLName("Freemyer-Ackerman");
+            peopleArray[0].SetPronouns("She/Her");
+            peopleArray[0].SetPType("Parent");
+            peopleArray[1].SetFName("Merida");
+            peopleArray[1].SetLName("Ackerman");
+            peopleArray[1].SetPronouns("She/Her");
+            peopleArray[1].SetPType("Student");
+            peopleArray[2].SetFName("Montserrat");
+            peopleArray[2].SetLName("Recarte-Pacheco");
+            peopleArray[2].SetPronouns("She/Her");
+            peopleArray[2].SetPType("Teacher");
+
+
+
+
+            
+
+
+            
+            //print each person to test the property gets and the toString
+            for (int index = 0; index < peopleArray.Length; index++)
+            {
+                if(!(((peopleArray[index]).GetFName())==null))
+                    Console.WriteLine(peopleArray[index]);
+            }
+
+        }
+    }
+}
