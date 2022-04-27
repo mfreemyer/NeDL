a429ade346a86db3a35d4d90aa7c083d7cdfb7f4
Maggie Freemyer-Ackerman <mfreemyer@gmail.com>
2022-04-27 09:26:09 -0500

    working on ListInterfaceCRUD, P works so far.

---
+++ ListInterfaceCRUD/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ListInterfaceCRUD
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // create a list of employees
+            List<Employee> employeeList = new List<Employee>();
+
+            // add some employees to the list to test
+            employeeList.Add(new Employee ("Maggie", "Freemyer-Ackerman", "S"));
+            employeeList.Add(new Employee ("Macy", "Craig", "H"));
+
+            // add some salaried employees to the list to test
+            employeeList.Add(new SalaryEmployee ("Rand", "Ackerman", "S", 75000));
+            employeeList.Add(new SalaryEmployee ("Doyle", "Freemyer", "S", 75000));            
+
+            // print the list
+            foreach (Employee anEmployee in employeeList)
+            {
+                Console.WriteLine(anEmployee);
+            }
+
+            // add an employee
+            
+        }
+    }
+}
