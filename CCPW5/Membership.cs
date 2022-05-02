676d2acd7c97584adc66187719452b6b759337b1
Maggie Freemyer-Ackerman <mfreemyer@gmail.com>
2022-05-02 11:14:57 -0500

    CCPW5 -- commit with hard coded Membership data. now switch Membership to abstract.

--- CCPW5/Membership.cs
+++ CCPW5/Membership.cs
@@ -3,9 +3,56 @@
 namespace CCPW5
 {
 
-    class Membership
+    /* abstract  */class Membership
     {
+        // define properties
+        public int memberID
+        { get; set; }
+        public string email
+        { get; set; }
+        public char memberType
+        { get; set; }
+        public double annualCost
+        { get; set; }
+        public double currentPFM // current amount of Purchases For the Month
+        { get; set; }
 
+        // default constructor
+        public Membership()
+        {
+            memberID = 0;
+            email = "";
+            memberType = '?';
+            annualCost = 0.0;
+            currentPFM = 0.0;
+        }
+
+        // constructor for when values are passed
+        public Membership(int newMemberID, string newEmail, char newMemberType, double newAnnualCost, double newCurrentPFM)
+        {
+            memberID = newMemberID;
+            email = newEmail;
+            memberType = newMemberType;
+            annualCost = newAnnualCost;
+            currentPFM = newCurrentPFM;
+        }
+
+
+        // methods
+        /* public double Purchase(); */
+
+
+        /* public double Return(); */
+
+
+        /* public abstract double ApplyCBR(); */ // apply cash-back rewards
+
+
+        // to string
+        public override string ToString()
+        {
+            return "Member ID: " + memberID + "  | Email: " + email + "  | Member Type: " + memberType + "  | Current Purchases for Month: " + currentPFM;
+        }
 
     }
 }
--- CCPW5/Program.cs
+++ CCPW5/Program.cs
@@ -9,6 +9,25 @@ namespace CCPW5
         static void Main(string[] args)
         {
             Console.WriteLine("Hello NeDL!");
+            // create lists and list objects
+            List<Membership> memberList = new List<Membership>(); // this list needs to be deleted after testing Membership class (for abstraction)
+
+
+            // hard code the test data into the lists
+            memberList.Add(new Membership (123, "123@gmail.com", 'R', 60, 162.73));
+            memberList.Add(new Membership (100, "100@gmail.com", 'E', 120, 417.56));
+            memberList.Add(new Membership (200, "200@gmail.com", 'B', 90, 6248.01));
+            memberList.Add(new Membership (300, "300@gmail.com", 'N', 50, 2062.93));
+
+            // print the lists
+            Console.WriteLine(); // for user-friendliness in terminal
+            foreach (Membership aMembership in memberList)
+            { Console.WriteLine(aMembership); }
+
+
+
+
+
         }
     }
 }
