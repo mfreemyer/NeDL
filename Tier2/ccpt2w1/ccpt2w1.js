d27d0e5e66acfe65cd8dea6617def7b5f80fa118
Maggie Freemyer-Ackerman <mfreemyer@gmail.com>
2022-05-12 18:23:10 -0500

    ccpt2w1 -- working on delete

--- Tier2/ccpt2w1/ccpt2w1.js
+++ Tier2/ccpt2w1/ccpt2w1.js
@@ -11,4 +11,17 @@ function addItem() {
         var tableRef = document.getElementById("toDoTable");
         (tableRef.insertRow(tableRef.rows.length)).innerHTML = newItem
     }
+
+    function deleteItem() {
+        for (aRow = 0; aRow < (tableRef.rows.length); aRow++) {
+            /* document.getElementById("tableRef").deleteRow((tableRef.rows[aRow]).innerHTML === newItem); */
+            if ((tableRef.rows[aRow]).innerHTML == addItem) {
+                
+                /* document.getElementById("tableRef").deleteRow(0); */
+                /* (tableRef.updateRow(tableRef.rows.length)).innerHTML ="" */
+                /* (tableRef.rows[aRow]).innerHTML = ""; */
+                /* addItem + " -- this is the item to delete."; */
+            } 
+        }
+    }
 }
