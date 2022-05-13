2fc5f28224e1d42226b9d8fabd1cda2b1d52d8ec
Maggie Freemyer-Ackerman <mfreemyer@gmail.com>
2022-05-13 17:00:18 -0500

    ccpt2w1 -- STILL working on delete. committing before trying something.

--- Tier2/ccpt2w1/ccpt2w1.ts
+++ Tier2/ccpt2w1/ccpt2w1.ts
@@ -1,5 +1,10 @@
 // addItem(), deleteItem(), checkItem()
 
+const createId = (str) => {
+    return `id_${str.replace(/ /ig, '_')}`;
+  }
+
+
 function addItem() {
     var newItem = document.forms["manageItems"]["toDoItem"].value;
 
@@ -15,15 +20,33 @@ function addItem() {
     }
 }
 
+
+
 function deleteItem() {
-    /* var tableRef = document.getElementById("toDoTable") as HTMLTableElement; */
+    var tableRef = document.getElementById("toDoTable") as HTMLTableElement;
     var itemToRemove = document.forms["manageItems"]["toDoItem"].value;
+    if (itemToRemove == "") {
+        alert("Please enter an item to delete.")
+        return false;
+    }
+    else {
+
+
+
+
+        var deleted = ""
+        deleted = document.forms["manageItems"]["toDoItem"].value;
+        document.forms["manageItems"]["toDoItem"].value = ""; // clears the input field after the value has been added to the list.
+        alert("Keep trying. You'll get it figured out." ) 
+        
+    }
 
     /* (tableRef.deleteRow(tableRef.rows.length)).innerHTML = newItem */
     /* document.getElementById("tableRef").deleteRow("itemToRemove"); */
     /* document.getElementById("tableRef").innerHTML = document.getElementById("tableRef").innerHTML.replace(itemToRemove,itemToRemove.strike()); */
     /* document.getElementById("tableRef").innerHTML.deleteRow(0); */ // trying to delete first row, just to see if i can...
-    alert("Whyyyyy can't I figure out the delete?!?!?!" ) 
+    
+    /* itemToRemove.value = ""; */
     
     
     
