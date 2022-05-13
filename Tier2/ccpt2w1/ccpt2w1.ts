07ec3df39ed56bba27b7fb9fe2f3b1d0bc05e785
Maggie Freemyer-Ackerman <mfreemyer@gmail.com>
2022-05-13 12:38:47 -0500

    ccpt2w1 -- still working on delete.

--- Tier2/ccpt2w1/ccpt2w1.ts
+++ Tier2/ccpt2w1/ccpt2w1.ts
@@ -1,28 +1,36 @@
 // addItem(), deleteItem(), checkItem()
+
 function addItem() {
     var newItem = document.forms["manageItems"]["toDoItem"].value;
+
     if (newItem == "") {
-        alert("Please enter a to-do item.");
+        alert("Please enter a to-do item.")
         return false;
     }
     else {
-        var tableRef = document.getElementById("toDoTable");
-        (tableRef.insertRow(tableRef.rows.length)).innerHTML = newItem;
-        /* document.forms["manageItems"]["toDoItem"] = ""; */
-        document.forms["manageItems"]["toDoItem"].value = "";
+        var tableRef = document.getElementById("toDoTable") as HTMLTableElement;
+        (tableRef.insertRow(tableRef.rows.length)).innerHTML = newItem
+        document.forms["manageItems"]["toDoItem"].value = ""; // clears the input field after the value has been added to the list.
         /* return true; */
     }
 }
+
 function deleteItem() {
     /* var tableRef = document.getElementById("toDoTable") as HTMLTableElement; */
     var itemToRemove = document.forms["manageItems"]["toDoItem"].value;
+
     /* (tableRef.deleteRow(tableRef.rows.length)).innerHTML = newItem */
     /* document.getElementById("tableRef").deleteRow("itemToRemove"); */
     /* document.getElementById("tableRef").innerHTML = document.getElementById("tableRef").innerHTML.replace(itemToRemove,itemToRemove.strike()); */
     /* document.getElementById("tableRef").innerHTML.deleteRow(0); */ // trying to delete first row, just to see if i can...
-    alert("Whyyyyy can't I figure out the delete?!?!?!");
+    alert("Whyyyyy can't I figure out the delete?!?!?!" ) 
+    
+    
+    
+
 }
+
 function clearList() {
     var tableRef = document.getElementById("toDoTable");
-    tableRef.innerHTML = "";
+    tableRef.innerHTML = ""; 
 }
