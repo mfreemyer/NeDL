e9194a0f9b72193aa849fb069af19a641a9c1fa6
Maggie Freemyer-Ackerman <mfreemyer@gmail.com>
2022-05-17 14:04:25 -0500

    ccpt2w2 -- pulling from api, but not in wrong format

--- Tier2/ccpt2w2/ccpt2w2.js
+++ Tier2/ccpt2w2/ccpt2w2.js
@@ -1,11 +1,30 @@
-async function getDictionary() {
+async function getDefinition() {
     // first build the API call string by starting with the URL
     var apiString = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-
-    // next add the parameters to the string using the drop down lists
     
 
+    // next add the parameters to the string using the drop down lists
+    var theWord = document.getElementById("word").value;
+    apiString = apiString + theWord;
+    alert(apiString);
+
     // now make the API call to the web service using the string and store what is returned in response
+    var response = await fetch(apiString);
+    
+    
+    // fnally, print the response
+    document.getElementById("wordDefinition").innerHTML = ""; // clear what was previously shown
+    alert("so far so good!")
+
+    var jsonData = await response.json(); // read the response as JSON
+
+    // stringify and print out the JSON object in the RawData section
+    document.getElementById("wordDefinition").innerHTML = JSON.stringify(jsonData);
+
+    return true;
+
+
+
 
 
     // finally, have fun with the data
