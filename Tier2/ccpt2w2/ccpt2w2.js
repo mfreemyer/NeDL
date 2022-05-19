d9fe072bfa8099f9a6271158fe983a24dbb757c0
Maggie Freemyer-Ackerman <mfreemyer@gmail.com>
2022-05-19 13:55:43 -0500

    ccpt2w2 -- committing before trying an if statement to include any synonyms

--- Tier2/ccpt2w2/ccpt2w2.js
+++ Tier2/ccpt2w2/ccpt2w2.js
@@ -5,55 +5,28 @@ async function getDefinitions() {
     // next add the parameters to the string using the drop down lists
     var theWord = document.getElementById("word").value;
     apiString = apiString + theWord/*  + "/" + "meanings" */;
-    /* alert(apiString); */
 
     // now make the API call to the web service using the string and store what is returned in response
     var response = await fetch(apiString);
     
-    
     // fnally, print the response
     document.getElementById("wordData").innerHTML = ""; // clear what was previously shown
-    
-
     var jsonData = await response.json(); // read the response as JSON
-    
 
-
-    // stringify and print out the JSON object in the RawData section  -- don't need this since not looking to print raw data
-    /* document.getElementById("wordData").innerHTML = JSON.stringify(jsonData); */
-
-    // loop through the JSON object one meaning at a time and print each 
-    // *** could do if statement for example, do for loop for multiple definitions ***
-
+    // loop through the JSON object one word, meaning, part of speech, definitions, and definition at a time and print each 
+    // *** could do an if statement for printing keys that don't always have a value.
     for (var aWord in jsonData) {
         let numMeanings = jsonData[aWord].meanings.length;
         for (var aMeaning  = 0; aMeaning < numMeanings; aMeaning++) {
-            document.getElementById("wordData").innerHTML += "<p>" + jsonData[aWord].meanings[aMeaning].partOfSpeech + "</p>";
+            document.getElementById("wordData").innerHTML += "<h4>" + jsonData[aWord].meanings[aMeaning].partOfSpeech + "</h4>";
             
             let numDefinitions = jsonData[aWord].meanings[aMeaning].definitions.length;
             for (var aDefinition = 0; aDefinition < numDefinitions; aDefinition++) {
-                document.getElementById("wordData").innerHTML += "<p>" + jsonData[aWord].meanings[aMeaning].definitions[aDefinition].definition + "</p>";
+                document.getElementById("wordData").innerHTML += "<ol start=1>" + jsonData[aWord].meanings[aMeaning].definitions[aDefinition].definition + "</ol>";
             }
-
-        }
-        
+        }  
     }
-
-
-    /* var theNewDefinition = "";
-    for (var aDefinition in jsonData) {
-          theNewDefinition += "<p><a href=" + jsonData[aDefinition].html_url + ">" + jsonData[aDefinition].theWord + "</a></p>";
-        }
-        
-        document.getElementById("word").innerHTML = theWord;
-        document.getElementById("wordData").innerHTML = theNewDefinition;
-        alert("so far so good!");
-        document.getElementById("word").value = ""; */
-        
-
-
+    document.getElementById("word").innerHTML = ""; // clear the value that was entered
+    /* alert("so far so good!"); */
     return true;
-
-
-
 }
