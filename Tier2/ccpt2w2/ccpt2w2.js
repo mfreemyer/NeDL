acc2ed69499397569f68136c61a605431827ebd7
Maggie Freemyer-Ackerman <mfreemyer@gmail.com>
2022-05-19 11:18:13 -0500

    ccpt2w2 -- committing after definitions are pulling and elements and variables renamed to make more sense.

--- Tier2/ccpt2w2/ccpt2w2.js
+++ Tier2/ccpt2w2/ccpt2w2.js
@@ -5,36 +5,35 @@ async function getDefinitions() {
     // next add the parameters to the string using the drop down lists
     var theWord = document.getElementById("word").value;
     apiString = apiString + theWord/*  + "/" + "meanings" */;
-    alert(apiString);
+    /* alert(apiString); */
 
     // now make the API call to the web service using the string and store what is returned in response
     var response = await fetch(apiString);
     
     
     // fnally, print the response
-    document.getElementById("wordDefinitions").innerHTML = ""; // clear what was previously shown
+    document.getElementById("wordData").innerHTML = ""; // clear what was previously shown
     
 
     var jsonData = await response.json(); // read the response as JSON
     
 
 
-    // stringify and print out the JSON object in the RawData section
-    /* document.getElementById("wordDefinitions").innerHTML = JSON.stringify(jsonData); */
+    // stringify and print out the JSON object in the RawData section  -- don't need this since not looking to print raw data
+    /* document.getElementById("wordData").innerHTML = JSON.stringify(jsonData); */
 
-    // loop through the JSON object one paragraph at a time and print each 
-    // *** LOOK INTO USING flatMap. 
+    // loop through the JSON object one meaning at a time and print each 
+    // *** could do if statement for example, do for loop for multiple definitions ***
 
-    
-    for (var aDefinition in jsonData) {
-        let numMeanings = jsonData[aDefinition].meanings.length;
+    for (var aWord in jsonData) {
+        let numMeanings = jsonData[aWord].meanings.length;
         for (var aMeaning  = 0; aMeaning < numMeanings; aMeaning++) {
-            document.getElementById("wordDefinitions").innerHTML += "<p>" + jsonData[aDefinition].meanings[aMeaning].partOfSpeech + "</p>";
-            document.getElementById("wordDefinitions").innerHTML += "<p>" + jsonData[aDefinition].meanings[aMeaning].definitions[0].definition + "</p>";
+            document.getElementById("wordData").innerHTML += "<p>" + jsonData[aWord].meanings[aMeaning].partOfSpeech + "</p>";
             
-
-            // could do if statement for example, do for loop for multiple definitions
-
+            let numDefinitions = jsonData[aWord].meanings[aMeaning].definitions.length;
+            for (var aDefinition = 0; aDefinition < numDefinitions; aDefinition++) {
+                document.getElementById("wordData").innerHTML += "<p>" + jsonData[aWord].meanings[aMeaning].definitions[aDefinition].definition + "</p>";
+            }
 
         }
         
@@ -47,7 +46,7 @@ async function getDefinitions() {
         }
         
         document.getElementById("word").innerHTML = theWord;
-        document.getElementById("wordDefinitions").innerHTML = theNewDefinition;
+        document.getElementById("wordData").innerHTML = theNewDefinition;
         alert("so far so good!");
         document.getElementById("word").value = ""; */
         
