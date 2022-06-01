d722efa42b5ae93a035a85fd4e532c47cca6cca4
Maggie Freemyer-Ackerman <mfreemyer@gmail.com>
2022-06-01 11:20:19 -0500

    committing before updating TodoItemsControllerto use TodoItemDTO

--- Tier2/TodoApi/TodoApi/Models/TodoItem.cs
+++ Tier2/TodoApi/TodoApi/Models/TodoItem.cs
@@ -3,7 +3,9 @@
     public class TodoItem
     {
         public long Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public bool IsComplete { get; set; }
+        public string? Secret { get; set; } // The secret field needs to be hidden from this app,
+                                            // but an administrative app could choose to expose it.
     }
 }
