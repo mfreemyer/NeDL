c798bc0f3e0b7a9c47dbd33ed77c4179b806e035
Maggie Freemyer-Ackerman <mfreemyer@gmail.com>
2022-06-07 13:44:51 -0500

    CCPT2W5 -- committing before starting over on front end.

---
+++ Tier2/CCPT2W5_HIITExercises - Copy/CCPT2W5_HIITExercises/Controllers/ExercisesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CCPT2W5_HIITExercises.Models;
+
+namespace CCPT2W5_HIITExercises.Controllers
+{
+    [Route("api/hiitexercises")]
+    [ApiController]
+    public class ExercisesController : ControllerBase
+    {
+        private readonly ExerciseContext _context;
+
+        public ExercisesController(ExerciseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Exercises
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Exercise>>> GetExercises()
+        {
+          if (_context.Exercises == null)
+          {
+              return NotFound();
+          }
+            return await _context.Exercises.ToListAsync();
+        }
+
+        // GET: api/Exercises/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Exercise>> GetExercise(long id)
+        {
+          if (_context.Exercises == null)
+          {
+              return NotFound();
+          }
+            var exercise = await _context.Exercises.FindAsync(id);
+
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+
+            return exercise;
+        }
+
+        // PUT: api/Exercises/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutExercise(long id, Exercise exercise)
+        {
+            if (id != exercise.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(exercise).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExerciseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Exercises
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Exercise>> PostExercise(Exercise exercise)
+        {
+          //if (_context.Exercises == null)
+          //{
+          //    return Problem("Entity set 'ExerciseContext.Exercises'  is null.");
+          //}
+          //  _context.Exercises.Add(exercise);
+          //  await _context.SaveChangesAsync();
+
+          //  return CreatedAtAction("GetExercise", new { id = exercise.Id }, exercise);
+
+            _context.Exercises.Add(exercise);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetExercise), new { id = exercise.Id }, exercise);
+        }
+
+        // DELETE: api/Exercises/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExercise(long id)
+        {
+            if (_context.Exercises == null)
+            {
+                return NotFound();
+            }
+            var exercise = await _context.Exercises.FindAsync(id);
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+
+            _context.Exercises.Remove(exercise);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ExerciseExists(long id)
+        {
+            return (_context.Exercises?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Tier2/CCPT2W5_HIITExercises/CCPT2W5_HIITExercises/wwwroot/site.js
@@ -0,0 +1,127 @@
+﻿const uri = 'api/hiitexercises';
+let exercises = [];
+
+function getItems() {
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => _displayItems(data))
+        .catch(error => console.error('Unable to get items.', error));
+}
+
+function addItem() {
+    const addNameTextbox = document.getElementById('add-Name');
+
+    const item = {
+        isComplete: false,
+        name: addNameTextbox.value.trim()
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(response => response.json())
+        .then(() => {
+            getItems();
+            addNameTextbox.value = '';
+        })
+        .catch(error => console.error('Unable to add item.', error));
+}
+
+function deleteItem(id) {
+    fetch(`${uri}/${id}`, {
+        method: 'DELETE'
+    })
+        .then(() => getItems())
+        .catch(error => console.error('Unable to delete item.', error));
+}
+
+function displayEditForm(id) {
+    const item = exercises.find(item => item.id === id);
+
+    document.getElementById('edit-Name').value = item.name;
+    document.getElementById('edit-Id').value = item.Id;
+    //accidentally deleted code for checkbox instead of commenting it out. refer to TodoApi if adding new checkbox.
+    document.getElementById('edit-PMG').value = item.PMG;
+    document.getElementById('edit-Equipment').value = item.Equipment;
+    document.getElementById('editForm').style.display = 'block';
+}
+
+function updateItem() {
+    const itemId = document.getElementById('edit-Id').value;
+    const item = {
+        id: parseInt(itemId, 10),
+        /*isComplete: document.getElementById('edit-isComplete').checked,*/
+        name: document.getElementById('edit-Name').value.trim()
+    };
+
+    fetch(`${uri}/${itemId}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(() => getItems())
+        .catch(error => console.error('Unable to update item.', error));
+
+    closeInput();
+
+    return false;
+}
+
+function closeInput() {
+    document.getElementById('editForm').style.display = 'none';
+}
+
+function _displayCount(itemCount) {
+    const name = (itemCount === 1) ? 'to-do' : 'to-dos';
+
+    document.getElementById('counter').innerText = `${itemCount} ${name}`;
+}
+
+function _displayItems(data) {
+    const tBody = document.getElementById('exercises');
+    tBody.innerHTML = '';
+
+    _displayCount(data.length);
+
+    const button = document.createElement('button');
+
+    data.forEach(item => {
+        let isCompleteCheckbox = document.createElement('input');
+        isCompleteCheckbox.type = 'checkbox';
+        isCompleteCheckbox.disabled = true;
+        isCompleteCheckbox.checked = item.isComplete;
+
+        let editButton = button.cloneNode(false);
+        editButton.innerText = 'Edit';
+        editButton.setAttribute('onclick', `displayEditForm(${item.id})`);
+
+        let deleteButton = button.cloneNode(false);
+        deleteButton.innerText = 'Delete';
+        deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
+
+        let tr = tBody.insertRow();
+
+        let td1 = tr.insertCell(0);
+        td1.appendChild(isCompleteCheckbox);
+
+        let td2 = tr.insertCell(1);
+        let textNode = document.createTextNode(item.name);
+        td2.appendChild(textNode);
+
+        let td3 = tr.insertCell(2);
+        td3.appendChild(editButton);
+
+        let td4 = tr.insertCell(3);
+        td4.appendChild(deleteButton);
+    });
+
+    exercises = data;
+}
